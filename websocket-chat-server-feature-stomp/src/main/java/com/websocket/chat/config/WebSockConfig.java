8a9bdb56a46938b77de28146fdc0d5d5aac49524
wlrhkd49 <wlrhkd49@naver.com>
2021-04-29 21:05:21 +0900

    finish stomp

---
+++ websocket-chat-server-feature-stomp/src/main/java/com/websocket/chat/config/WebSockConfig.java
@@ -0,0 +1,29 @@
+package com.websocket.chat.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSockConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        //메세지 구동 요청의 prefix /sub로 시작 설정
+        config.enableSimpleBroker("/sub");
+        // 메세지를 발행하는 요청의 prefix는 /pub로 시작 설정
+        config.setApplicationDestinationPrefixes("/pub");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // stomp websocket의 연결 endpoint는 /ws-stomp로 설정
+        // 도메인이 다른 서버에서도 접속 가능하도록 setAllowedOrigin("*")설정
+        // 개발서버의 접속 주소 ws://localhost:8080/ws-stomp
+        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+                .withSockJS();
+    }
+}
---
+++ websocket-chat-server-feature-stomp/src/main/java/com/websocket/chat/repo/ChatRoomRepository.java
@@ -0,0 +1,37 @@
+package com.websocket.chat.repo;
+
+import com.websocket.chat.model.ChatRoom;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Repository
+public class ChatRoomRepository {
+
+    // 서비스에서는 DB나 다른 저장 매체에 채팅방 정보를 저장하도록 구현해야함.
+    private Map<String, ChatRoom> chatRoomMap;
+
+    @PostConstruct // 초기화 작업을 할 메소드에 적용. WAS가 띄워질 때 실행
+    private void init() {
+        chatRoomMap = new LinkedHashMap<>();
+    }
+
+    public List<ChatRoom> findAllRoom() {
+        // 채팅방 생성순서 최근 순으로 반환
+        List chatRooms = new ArrayList<>(chatRoomMap.values());
+        Collections.reverse(chatRooms);
+        return chatRooms;
+    }
+
+    public ChatRoom findRoomById(String id) {
+        return chatRoomMap.get(id);
+    }
+
+    public ChatRoom createChatRoom(String name) {
+        ChatRoom chatRoom = ChatRoom.create(name);
+        chatRoomMap.put(chatRoom.getRoomId(), chatRoom);
+        return chatRoom;
+    }
+}
